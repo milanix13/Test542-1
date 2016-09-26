6ea22deb48d9f80f25755dc60f85ae1c5a925643
Mila Contreras <milanix13@gmail.com>
2016-09-25 20:01:02 -0700

    Updated using DateTime functions.

--- test-date-calc.php
+++ test-date-calc.php
@@ -16,16 +16,17 @@ class DateCalculatorTest extends WP_UnitTestCase {
 
 	function test_date_add_func() {
 		$arr = array();
+		$datetime = new DateTime('tomorrow');
 
 		// Checking date
 		$arr['day'] = 1;
 		$result = date_add_func($arr);
-		$this->assertEquals( $result, '26.09.2016' );
+		$this->assertEquals( $result, $datetime->format('d.m.Y') );
 
 		// Checking format
 		$arr['format'] = 'm/d/Y';
 		$result = date_add_func($arr);
-		$this->assertEquals( $result, '09/26/2016' );
+		$this->assertEquals( $result, $datetime->format($arr['format']) );
 	}
 
 	/**
@@ -34,16 +35,17 @@ class DateCalculatorTest extends WP_UnitTestCase {
 
 	function test_date_sub_func() {
 		$arr = array();
+		$datetime = new DateTime('yesterday');
 
 		// Checking date
 		$arr['day'] = 1;
 		$result = date_sub_func($arr);
-		$this->assertEquals( $result, '24.09.2016' );
+		$this->assertEquals( $result, $datetime->format('d.m.Y') );
 
 		// Checking format
 		$arr['format'] = 'm/d/Y';
 		$result = date_sub_func($arr);
-		$this->assertEquals( $result, '09/24/2016' );
+		$this->assertEquals( $result, $datetime->format($arr['format']) );
 	}
 
 	/**
@@ -52,31 +54,32 @@ class DateCalculatorTest extends WP_UnitTestCase {
 
 	function test_date_now_func() {
 		$arr = array();
+		$datetime = new DateTime('now');
 
 		// Checking day
 		$arr['show'] = 'dayonly';
 		$result = date_now_func($arr);
-		$this->assertEquals( $result, '25' );
+		$this->assertEquals( $result, $datetime->format('d') );
 
 		// Checking month
 		$arr['show'] = 'monthonly';
 		$result = date_now_func($arr);
-		$this->assertEquals( $result, '09' );
+		$this->assertEquals( $result, $datetime->format('m') );
 
 		// Checking year
 		$arr['show'] = 'yearonly';
 		$result = date_now_func($arr);
-		$this->assertEquals( $result, '2016' );
+		$this->assertEquals( $result, $datetime->format('Y') );
 
 		// Checking month
 		$arr['show'] = 'all';
 		$result = date_now_func($arr);
-		$this->assertEquals( $result, '26.09.2016' );
+		$this->assertEquals( $result, $datetime->format('d.m.Y') );
 
 		// Checking format
 		$arr['format'] = 'm/d/Y';
 		$result = date_now_func($arr);
-		$this->assertEquals( $result, '09/26/2016' );
+		$this->assertEquals( $result, $datetime->format($arr['format']) );
 	}
 
 	/**
@@ -85,17 +88,18 @@ class DateCalculatorTest extends WP_UnitTestCase {
 
 	function test_date_say_func() {
 		$arr = array();
+		$datetime = new DateTime('next tuesday');
 
 		// Checking date
 		$arr['what'] = 'next tuesday';
 		$arr['show'] = 'all';
 		$result = date_add_func($arr);
-		$this->assertEquals( $result, '04.10.2016' );
+		$this->assertEquals( $result, $datetime->format('d.m.Y') );
 
 		// Checking format
 		$arr['show'] = 'all';
 		$arr['format'] = 'm/d/Y';
 		$result = date_add_func($arr);
-		$this->assertEquals( $result, '10/04/2016' );
+		$this->assertEquals( $result, $datetime->format($arr['format']) );
 	}
 }
